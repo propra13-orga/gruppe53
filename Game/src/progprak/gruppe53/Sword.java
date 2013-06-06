85b4e07de94e4b8262c861b66649bf47cffcddd0
Marcel <Marcel@10.84.30.241>
2013-06-06 14:47:28 +0200

    Added Sword and Item Classes

---
+++ Game/src/progprak/gruppe53/Sword.java
@@ -0,0 +1,19 @@
+package progprak.gruppe53;
+
+public class Sword extends Items implements Collidable {
+
+	private static final long serialVersionUID = 123456789L;
+
+	private CollisionEvent collisionEvent;
+	
+	public Sword(int x, int y){
+		super( x, y, "images/sword1.png");
+		collisionEvent = new CollisionEvent(CollisionEvent.EVENT_DAMAGE);
+	}
+	
+	@Override
+	public void doLogic(long delta){
+		
+	}
+	
+}
