f893d3b058eeefcd8acfa60c83050b7fdbe04aac
ORohr <oliver.rohr@uni-duesseldorf.de>
2013-06-06 15:18:17 +0200

    Added ManaBar in InfoWindow, getFunctions for Manabar

--- Game/src/progprak/gruppe53/InfoWindow.java
+++ Game/src/progprak/gruppe53/InfoWindow.java
@@ -3,6 +3,7 @@ package progprak.gruppe53;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
@@ -14,6 +15,13 @@ public class InfoWindow extends JPanel {
 
 	private JProgressBar healthBar;
 	
+	private JProgressBar manaBar;
+	
+	private JPanel barPanel;
+	
+	private InventoryPanel inventoryPanel;
+	
+	
 	public InfoWindow(Game game) {
 		this.game = game;
 		doInitalizations();
@@ -21,19 +29,32 @@ public class InfoWindow extends JPanel {
 
 	private void doInitalizations() {
 		setBackground(Color.WHITE);
+		setLayout(new BorderLayout());
+		barPanel = new JPanel();
+		barPanel.setLayout(new BoxLayout(barPanel, BoxLayout.PAGE_AXIS));
 		healthBar = new JProgressBar();
+		manaBar = new JProgressBar();
+		inventoryPanel = new InventoryPanel();
 		healthBar.setForeground(Color.RED);
-		add(healthBar,BorderLayout.NORTH);
-		Dimension ps = healthBar.getPreferredSize();
-		ps.width = 800;
-		healthBar.setPreferredSize(ps);
-		
+		manaBar.setForeground(Color.BLUE);
+		barPanel.add(healthBar);
+		barPanel.add(manaBar);
+		Dimension hpb = healthBar.getPreferredSize();
+		Dimension mpb = manaBar.getPreferredSize();
+		hpb.width = 800;
+		mpb.width = 800;
+		healthBar.setPreferredSize(hpb);
+		manaBar.setPreferredSize(mpb);
+		add(barPanel,BorderLayout.NORTH);
+		add(inventoryPanel,BorderLayout.SOUTH);
 		setPreferredSize(getPreferredSize());
 	}
 
 	public void render() {
 		healthBar.setMaximum(game.getGameLogic().getHero().getMaxHealth());
 		healthBar.setValue(game.getGameLogic().getHero().getHealth());
+		manaBar.setMaximum(game.getGameLogic().getHero().getMaxMana());
+		manaBar.setValue(game.getGameLogic().getHero().getMana());
 	}
 
 }
