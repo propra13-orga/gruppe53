33a73080403b36b7ad0febff38f9f9e7c4f3509c
ORohr <oliver.rohr@uni-duesseldorf.de>
2013-07-10 15:02:19 +0200

    Comments for Inventory

--- Game/src/progprak/gruppe53/game/Inventory.java
+++ Game/src/progprak/gruppe53/game/Inventory.java
@@ -23,14 +23,23 @@ public class Inventory implements Serializable {
 		this.gameLogic = gameLogic;
 	}
 
+	/**
+	 * @return returns Weapon from Weaponslot
+	 */
 	public Weapon getWeapon() {
 		return weapon;
 	}
 
+	/**
+	 * @return returns Armor from Armorslot
+	 */
 	public Armor getArmor() {
 		return armor;
 	}
 
+	/**
+	 * @return returns true, if the Inventory has a empty Slot
+	 */
 	public boolean hasFreeSlot() {
 		for(int i=0;i<inventorySlots;i++){
 			if(items[i] == null){
@@ -40,6 +49,10 @@ public class Inventory implements Serializable {
 		return false;
 	}
 
+	/**
+	 * @param Required new Item for Inventory
+	 * Adds the new Item to an empty InventorySlot
+	 */
 	public void addItem(Item item) {
 		for(int i=0;i<inventorySlots;i++){
 			if(items[i] == null){
@@ -49,29 +62,40 @@ public class Inventory implements Serializable {
 		}
 	}
 
+	/**
+	 * @return Returns all Items
+	 */
 	public Item[] getItems() {
 		return items;
 	}
 
+	/**
+	 * @param Required the SlotNumber
+	 * Uses the Item in the Inventoryslot with the SlotNumber
+	 */
 	public void slotClicked(int slotNumber) {
 		if(items[slotNumber] != null){
+			// if the Slot is empty, the click has no effect
 			if(hero.isShopOpen()){
 				hero.setMoney(hero.getMoney() + items[slotNumber].getPrice());
 				items[slotNumber] = null;
 			}
 			else {
+				// if the Slot contains a Weapon, it is added to the Weaponslot
 				if(items[slotNumber] instanceof Weapon){
 					items[inventorySlots] = weapon;
 					weapon = (Weapon) items[slotNumber];
 					items[slotNumber] = items[inventorySlots];
 					gameLogic.addSprite(weapon);
 				}
+				// if the Slot contains an Armor , it is added to the Armorslot
 				else if(items[slotNumber] instanceof Armor){
 					items[inventorySlots] = armor;
 					armor = (Armor) items[slotNumber];
 					items[slotNumber] = items[inventorySlots];
 					gameLogic.addSprite(armor);
 				}
+				// if the Slot contains a useable Item, it is used and removed from the Inventory
 				else{
 					items[slotNumber] .use();
 					items[slotNumber] = null;
