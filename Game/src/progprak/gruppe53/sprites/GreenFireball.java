bd46ef0badd6da6bd3f2d291cd74c6a456399fa6
NiklasBickel <Niklas.Bickel@uni-duesseldorf.de>
2013-07-16 11:06:46 +0200

    Fireballs only turn once

--- Game/src/progprak/gruppe53/sprites/GreenFireball.java
+++ Game/src/progprak/gruppe53/sprites/GreenFireball.java
@@ -9,6 +9,7 @@ import progprak.gruppe53.game.Shooter;
 public class GreenFireball extends Projectile implements Collidable{
 	
 	private static final long serialVersionUID = 1L;
+	private double dx2;
 
 
 	
@@ -17,6 +18,7 @@ public class GreenFireball extends Projectile implements Collidable{
 		this.setWidth(16);
 		this.setHeight(16);
 		this.dx = dx;
+		dx2 = -1 * dx;
 		this.dy = dy;
 		this.faction = faction;
 		damageType = 4;
@@ -43,7 +45,7 @@ public class GreenFireball extends Projectile implements Collidable{
 		if (de.getFaction() == 1){
 			faction = 3;
 			collisionEvent.setActor(de.getActor());
-			dx *= -1;
+			dx = dx2;
 			faction = 3;
 			initCollisionEvent();
 		}
